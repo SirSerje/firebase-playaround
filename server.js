1278dec3d5926502a021e3a85394c35ff8672599
SirSerje <sir.serje@gmail.com>
2018-06-26 01:17:31 +0300

    update readme & dependencies, fix some bugs, +ES6

--- server.js
+++ server.js
@@ -1,48 +1,31 @@
 const admin = require("firebase-admin");
+const express = require("express");
 const serviceAccount = require("./secure/serviceAccountKey.json");
 const uid = "some-uid";
-const express = require("express");
 const app = express();
 const dbURL = "https://test-learn-ff.firebaseio.com";
 
 admin.initializeApp({
-    credential : admin.credential.cert(serviceAccount),
-    databaseURL: dbURL,
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: dbURL
 });
 
-app.get("/new", function (req, res) {
+app.get("/token", function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-    admin.auth().createCustomToken(uid)
-        .then(function (customToken) {
-            res.send(customToken);
-        })
-        .catch(function (error) {
-            console.log("Error creating custom token:", error);
-        });
-
+  admin
+    .auth()
+    .createCustomToken(uid)
+    .then(function(customToken) {
+      res.send(customToken);
+    })
+    .catch(function(error) {
+      console.log("Error creating custom token:", error);
+    });
 });
 
-//FIXME: this part of code should fix CORS issues I have now
 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+app.listen(3000, function() {
+  console.log("server is running on http://localhost:3000");
 });
-
-// app.get('/', function(req, res, next) {
-//     // Handle the get for this route
-// });
-//
-// app.post('/', function(req, res, next) {
-//     // Handle the post for this route
-// });
-
-app.listen(3000, function () {
-    console.log("server is running on http://localhost:3000");
-});
-
-//
-// function test() {
-//     console.log('asd')
-// }
